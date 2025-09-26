53d6645ddebbf9daf42f8d6b1a0a96fd9fa3c008
yunwei37 <yangyiwei2000@gmail.com>
2025-09-26 05:18:35 +0000

    update

--- arm_toolchain.cmake
+++ arm_toolchain.cmake
@@ -18,8 +18,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # ARM-specific compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a")
+set(CMAKE_C_FLAGS "-march=armv8-a")
+set(CMAKE_CXX_FLAGS "-march=armv8-a")
 
 # Define ARM64 architecture
 add_definitions(-D__aarch64__)
